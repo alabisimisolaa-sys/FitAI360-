f7accbc195c73e79c247a755fad5f91677f9bce7
Simisola Alabi <alabi.simisolaa@gmail.com>
2026-03-24 18:13:43 -0500

    Update next.config.mjs to include serverComponentsExternalPackages and merge existing serverActions configuration.

---
+++ next.config.mjs
@@ -0,0 +1,5 @@
+// Merged configuration for Next.js
+export default {
+  serverActions: true, // Existing configuration
+  serverComponentsExternalPackages: ['pdfkit'], // Added from next.config.js
+};
